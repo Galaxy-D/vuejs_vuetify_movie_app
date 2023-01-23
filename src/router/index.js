2e7327898efb410a2b7bf982239ec31f061e3f8a
BlackOut <lavoixlactee007@gmail.com>
2023-01-23 23:13:51 +0100

    new routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MovieDetails from '../components/MovieDetails.vue'
+import ActorsList from '../components/ActorsList.vue'
+import ActorDetails from '../components/ActorDetails.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,12 +15,32 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/movie/:id',
+    name: 'home',
+    component: MovieDetails
+  },
+  {
+    path: '/actors',
+    name: 'actors',
+    component: ActorsList
+  },
+  {
+    path: '/actor/:id',
+    name: 'actors',
+    component: ActorDetails
+  },
+  {
+    path: '/contact',
+    name: 'contact',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/TheContact.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "about" */ '../components/TheProfile.vue')
   }
 ]
 
